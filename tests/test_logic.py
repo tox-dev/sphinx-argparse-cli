b7fad51507b0c827173e76b51dc786475f3b9d72
Bernát Gábor <bgabor8@bloomberg.net>
2021-02-05 12:01:07 +0000

    Add test
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -7,14 +7,21 @@ from sphinx.testing.util import SphinxTestApp
 
 
 @pytest.mark.sphinx("html", testroot="basic")
-def test_basic_as_text(app: SphinxTestApp) -> None:
+def test_basic_as_html(app: SphinxTestApp) -> None:
     app.build()
     outcome = (Path(app.outdir) / "index.html").read_text()
     assert outcome
 
 
 @pytest.mark.sphinx("html", testroot="complex")
-def test_complex_as_text(app: SphinxTestApp) -> None:
+def test_complex_as_html(app: SphinxTestApp) -> None:
     app.build()
     outcome = (Path(app.outdir) / "index.html").read_text()
     assert outcome
+
+
+@pytest.mark.sphinx("text", testroot="prog")
+def test_prog_as_text(app: SphinxTestApp) -> None:
+    app.build()
+    outcome = (Path(app.outdir) / "index.txt").read_text()
+    assert outcome == "magic - CLI interface\n*********************\n\n   magic\n"
