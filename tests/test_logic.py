8bbd1cd0d7c647b774ef6b3d6b0ed51cb1658436
Bernát Gábor <bgabor8@bloomberg.net>
2021-02-13 09:13:36 +0000

    Improve test suite
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,39 +3,45 @@ from __future__ import annotations
 from pathlib import Path
 
 import pytest
+from _pytest.fixtures import SubRequest
 from sphinx.testing.util import SphinxTestApp
 
 
-@pytest.mark.sphinx("html", testroot="basic")
-def test_basic_as_html(app: SphinxTestApp) -> None:
+@pytest.fixture()
+def build_outcome(app: SphinxTestApp, request: SubRequest) -> str:
     app.build()
-    outcome = (Path(app.outdir) / "index.html").read_text()
-    assert outcome
+    ext = {"html": "html", "text": "txt"}
+    ext = ext.get(request.node.get_closest_marker("sphinx").args[0])
+    assert ext is not None
+    return (Path(app.outdir) / f"index.{ext}").read_text()
+
+
+@pytest.mark.sphinx("html", testroot="basic")
+def test_basic_as_html(build_outcome: str) -> None:
+    assert build_outcome
+
+
+@pytest.mark.sphinx("text", testroot="complex")
+def test_complex_as_text(build_outcome: str) -> None:
+    expected = (Path(__file__).parent / "complex.txt").read_text()
+    assert build_outcome == expected
 
 
 @pytest.mark.sphinx("html", testroot="complex")
-def test_complex_as_html(app: SphinxTestApp) -> None:
-    app.build()
-    outcome = (Path(app.outdir) / "index.html").read_text()
-    assert outcome
+def test_complex_as_html(build_outcome: str) -> None:
+    assert build_outcome
 
 
 @pytest.mark.sphinx("text", testroot="prog")
-def test_prog_as_text(app: SphinxTestApp) -> None:
-    app.build()
-    outcome = (Path(app.outdir) / "index.txt").read_text()
-    assert outcome == "magic - CLI interface\n*********************\n\n   magic\n"
+def test_prog_as_text(build_outcome: str) -> None:
+    assert build_outcome == "magic - CLI interface\n*********************\n\n   magic\n"
 
 
 @pytest.mark.sphinx("text", testroot="title-set")
-def test_set_title_as_text(app: SphinxTestApp) -> None:
-    app.build()
-    outcome = (Path(app.outdir) / "index.txt").read_text()
-    assert outcome == "My own title\n************\n\n   foo\n"
+def test_set_title_as_text(build_outcome: str) -> None:
+    assert build_outcome == "My own title\n************\n\n   foo\n"
 
 
 @pytest.mark.sphinx("text", testroot="title-empty")
-def test_empty_title_as_text(app: SphinxTestApp) -> None:
-    app.build()
-    outcome = (Path(app.outdir) / "index.txt").read_text()
-    assert outcome == "   foo\n"
+def test_empty_title_as_text(build_outcome: str) -> None:
+    assert build_outcome == "   foo\n"
