1b0dd944fc67c3e28ee77ed5dd99917b264cd9ef
Lukas Mayrhofer <lukas_mayrhofer@mailbox.org>
2024-02-17 20:21:51 +0100

    Add test for argparse.SUPPRESS

---
+++ roots/test-suppressed-action/parser.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser, SUPPRESS
+
+
+def make() -> ArgumentParser:
+    parser = ArgumentParser(prog="foo", description="desc", add_help=False)
+    parser.add_argument(
+        "--activities-since",
+        metavar="TIMESTAMP",
+        help=SUPPRESS,
+    )
+    return parser
