ddc99fac941d9c8683656609ef254802ace0207f
Lukas Mayrhofer <34736939+mayrholu@users.noreply.github.com>
2024-02-17 21:23:03 +0100

    Epilog support (#146)
    * Add support for epilog
    
    * Add epilog tests
    
    * Update README.md
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ roots/test-epilog-multiline/parser.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+
+def make() -> ArgumentParser:
+    return ArgumentParser(
+        prog="foo",
+        epilog="""This epilog
+spans multiple lines.
+
+  this line is indented.
+    and also this.
+
+Now this should be a separate paragraph.
+""",
+        formatter_class=RawDescriptionHelpFormatter,
+        add_help=False,
+    )
