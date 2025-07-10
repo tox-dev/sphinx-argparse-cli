13796de2db92e7cb781e378ac643c6cba5a88126
Bernát Gábor <bgabor8@bloomberg.net>
2025-07-10 09:24:14 -0700

    Fix CI
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_sphinx_argparse_cli.py
+++ tests/test_sphinx_argparse_cli.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 
 def test_version() -> None:
-    import sphinx_argparse_cli
+    import sphinx_argparse_cli  # noqa: PLC0415
 
     assert sphinx_argparse_cli.__version__
